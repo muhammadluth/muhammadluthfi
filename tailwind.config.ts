f468ccdeb8363e05d48d79ceee310ce5a4c65868
Muhammad Luthfi <muhammad.lutfi@artajasa.co.id>
2024-03-08 15:07:02 +0700

    fix: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,16 @@ const config: Config = {
     './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        'roboto-condensed': ['var(--robotoCondensed-fon)']
       },
     },
   },
