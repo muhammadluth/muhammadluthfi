9b4e3eb594f440585b5d916e1aebea7f2e2678e6
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2024-04-16 09:32:06 +0700

    fix: tidy up

--- src/lib/components/partials/layout/index.tsx
+++ src/lib/components/partials/layout/index.tsx
@@ -19,15 +19,13 @@ export default function Layout({ children }: LayoutProps) {
   }, []);
 
   return (
-    <div>
-      <div className={clsx("max-w-6xl mx-auto lg:px-8")}>
-        <div className="flex flex-col lg:flex-row lg:gap-5 lg:py-4 xl:pb-8">
-          <Sidebar />
-          <main className="lg:w-4/5 max-w-[854px] transition-all duration-300">
-            {children}
-          </main>
+    <main className={clsx("max-w-6xl mx-auto lg:px-8")}>
+      <div className="flex flex-col lg:flex-row lg:gap-5 lg:py-4 xl:pb-8">
+        <Sidebar />
+        <div className="lg:w-4/5 max-w-[854px] transition-all duration-300">
+          {children}
         </div>
       </div>
-    </div>
+    </main>
   );
 }
